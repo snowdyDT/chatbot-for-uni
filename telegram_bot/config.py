d6877c8adf30cfcc2dca6d355b6cfd9a0fc31626
Sabyr <shatekov.sabyr@gmail.com>
2023-02-19 03:26:08 +0600

    feat(telegram_bot): installed rasa library and test models(incorrect library versions) (2)

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -1,3 +1,7 @@
 import keyring
 
-TOKEN = keyring.get_password("TELEGRAM_BOT_RASA", "TOKEN")
+TOKEN = keyring.get_keyring("TELEGRAM_BOT_RASA", "TOKEN")
+
+RASA_MODEL_PATH = "../RASA MODELS/models/20230128-170349-parallel-proof.tar.gz"
+RASA_MODEL_ABSOLUTE_PATH = r"/Users/sabr/Desktop/chatbot_for_uni/RASA " \
+                           r"MODELS/models/20230128-170349-parallel-proof.tar.gz "
---
+++ telegram_bot/rasa_output.py
@@ -0,0 +1,26 @@
+import config
+from rasa.nlu.model import Interpreter
+
+# path of your model
+rasa_model_path = config.RASA_MODEL_ABSOLUTE_PATH
+
+# create an interpreter object
+interpreter = Interpreter.load(rasa_model_path)
+
+"""
+Function to get model output
+Args:
+  text  (string)  --  input text string to be passed)
+For example: if you are interested in entities, you can just write result['entities']
+Returns:
+  json  --  json output to used for accessing model output
+"""
+
+
+def rasa_output(text):
+    message = str(text).strip()
+    result = interpreter.parse(message)
+    return result
+
+
+rasa_output("<my-text-input-content-for-the-model>")
