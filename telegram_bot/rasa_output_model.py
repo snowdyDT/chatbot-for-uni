f376be2dcc840e922f20291e3dc37abd51b935a4
Sabyr <shatekov.sabyr@gmail.com>
2023-02-19 03:24:48 +0600

    feat(telegram_bot): installed rasa library and test models(incorrect library versions)

---
+++ telegram_bot/rasa_output_model.py
@@ -0,0 +1,23 @@
+import config
+import asyncio
+from rasa.core.agent import Agent
+from rasa.shared.utils.io import json_to_string
+
+
+class Model:
+
+    def __init__(self, model_path: str) -> None:
+        self.agent = Agent.load(model_path)
+        print("NLU model loaded")
+
+    def message(self, message: str) -> str:
+        message = message.strip()
+        result = asyncio.run(self.agent.parse_message(message))
+        print("aaaaa")
+        print(result)
+        return json_to_string(result)
+
+
+mdl = Model(config.RASA_MODEL_ABSOLUTE_PATH)
+sentence = "Привет"
+print(mdl.message(sentence))
